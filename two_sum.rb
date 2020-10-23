1b76395156f297e8a3f57b116176a96f262ef845
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-24 00:19:26 +0200

    Calculate the complexity of the bad_two_sum?

--- two_sum.rb
+++ two_sum.rb
@@ -1,8 +1,10 @@
+# Time Complexity: O(N^2)
+# Space Complexity: O(1)
 def bad_two_sum?(arr, target_sum)
     len = arr.length
-    (0...len).each do |i|
+    (0...len).each do |i| # O(N)
         cur = arr[i]
-        (0...len).each do |j|
+        (0...len).each do |j| # O(N)
             next if i == j
             return true if target_sum == cur + arr[j]
         end
