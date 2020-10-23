6052fa58aee472f4d2bfec65d35542fe87c42167
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-23 22:15:28 +0200

    Calculate the complexity of the first anagram

--- anagrams.rb
+++ anagrams.rb
@@ -1,8 +1,9 @@
+## O(N!)
 def first_anagram?(str1, str2)
     str1_arr = str1.chars
-    str1_anag = str1_arr.permutation.to_a
+    str1_anag = str1_arr.permutation.to_a # O(N!) --> N: Number of permutations in str1
     str2_arr = str2.chars
-    str1_anag.any? do |anag|
+    str1_anag.any? do |anag| # O(N)
         str2_arr == anag
     end
 end
