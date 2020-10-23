b214d2d86706eab5b7e7e3833b1702e2eab7a8bb
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-24 00:55:43 +0200

    Okay_two_sum? which is more efficent than the bad_two_sum?

--- two_sum.rb
+++ two_sum.rb
@@ -15,3 +15,16 @@ end
 arr = [0, 1, 5, 7]
 p bad_two_sum?(arr, 6) # => should be true
 p bad_two_sum?(arr, 10) # => should be false
+
+
+def okay_two_sum?(arr, target_sum)
+    sorted_arr = arr.dup.sort!
+    len = arr.length
+    (0...len-1).each do |i|
+        return true if target_sum == arr[i] + arr[i-1]
+    end
+    false
+end
+
+p okay_two_sum?(arr, 6) # => should be true
+p okay_two_sum?(arr, 10) # => should be false
